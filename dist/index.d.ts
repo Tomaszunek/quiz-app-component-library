c8ed9d42551d81b9863f7e8a3683c15b97f4897f
Tomaszunek <pawlowskitomaszu@gmail.com>
2022-08-27 12:30:33 +0200

    remove dist

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { AnswerText, QuestionText } from "./atoms";
-export { AnswersWrapper } from "./molecules";
-export { Question } from "./organisms";
-export { QuestionView } from "./templates";
